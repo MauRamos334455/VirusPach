4ed94e5b832d3c56d62b556e5bae7034051d747b
MauRamos334455 <mauri334455@comunidad.unam.mx>
2020-09-14 13:17:47 -0500

    Renombre a src/testClass.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from organism import Organism
-
-covid10 = Organism(123, 'hola', 'hola')
-covid20 = Organism(12, 'esto no', 'no lo se')
-
-with open('123.json') as file:
-    covid10.createByJSON(file, 'organisms', 'nc', 'sequence', 'description')
-covid10.printOrganism()
