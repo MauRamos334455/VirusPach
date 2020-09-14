c3af83081fe99841966962336ea65203c2ac109a
MauRamos334455 <mauri334455@comunidad.unam.mx>
2020-09-14 13:19:04 -0500

    Creación del directorio src

---
+++ src/organism.py
@@ -0,0 +1,113 @@
+import json
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+class Organism:
+    """ 
+    A class used to represented a sequence organism details.
+
+    ...
+
+    Attributes
+    ----------
+    nc: int
+        The identifier number of the organism
+    sequence: str
+        The genome sequence of the organism
+    description: str
+        The name and description of the organism
+
+    Methods
+    -------
+    createByJson(file)
+        Create a Organism object with the information
+        contained in the file given (.JSON)
+    createByFasta(file)
+        Create a Organism object with the information
+        contained in the file given (.FNA)
+    exportToFasta()
+        Create a file with extension .fna (FASTA file)
+        with the information of the organism
+    exportToJson()
+        Create a file with extension .json
+        with the information of the organism
+
+    """
+    def __init__(self, nc, sequence, description='None'):
+        """
+        Parameters
+        ----------
+        nc : int
+            The identifier number
+        sequence : str
+            The complete gnome
+        description : str, optional
+            The name or the description of the organism
+        """
+        if not isinstance(nc, int):
+           raise TypeError("The NC must be a number")
+        elif not isinstance(sequence, str):
+           raise TypeError("The sequence must be a string")
+        elif not isinstance(description, str):
+            raise TypeError("The description must be a string")
+        else:
+            self.nc = nc
+            self.sequence = sequence
+            self.description = description
+    
+    def __eq__(self, anorganism):
+        """
+        From another organism (anorganism) compares
+        the nc number, if is equal the result is true.
+
+        Parameters
+        ----------
+        anorganism : Organism object
+            The organism to compare
+        """
+        if isinstance(anorganism, Organism) == False:
+            raise TypeError("Just can compare organisms with organisms")
+        if self.nc == anorganism.nc:
+            return True
+        else:
+            return False
+
+    def printOrganism(self):
+        print("Description: "+self.description)
+        print("NC: "+str(self.nc))
+        print("Sequence: "+self.sequence)
+
+    def createByJSON(self, file, collection, nc, sequence, description):
+        data = json.load(file)
+        for p in data[collection]:
+            organism = Organism(int(p[nc]), p[sequence], p[description])
+            return organism
+
+    def createByFasta(self, namefile):
+        with open(namefile, "rU") as handle:
+            for record in SeqIO.parse(handle, "fasta"):
+                print(record.format)
+                
+    def exportToFasta(self):
+        record = SeqRecord(
+            Seq(self.sequence),
+            id=str(self.nc),
+            name="None",
+            description=self.description
+        )
+        sequences = []
+        sequences.append(record)
+        with open("example.fasta", "w") as output_handle:
+            SeqIO.write(sequences, output_handle, "fasta")
+
+    def exportToJson(self):
+        data = {}
+        data['organisms'] = []
+        data['organisms'].append({
+            'nc': str(self.nc),
+            'sequence': self.sequence,
+            'description': self.description
+        })
+        with open(str(self.nc)+'.json', 'w') as outfile:
+            json.dump(data, outfile)
---
+++ src/testClass.py
@@ -0,0 +1,6 @@
+from organism import Organism
+
+covid10 = Organism(123, 'hola', 'hola')
+covid20 = Organism(12, 'esto no', 'no lo se')
+
+covid20.exportToFasta()
