83ad061ed1b6a80930083ddb47fcb67ba01929e1
MauRamos334455 <mauri334455@comunidad.unam.mx>
2020-09-17 22:15:44 -0500

    cambio de NC por ID y anexo de version

--- src/organism.py
+++ src/organism.py
@@ -12,8 +12,10 @@ class Organism:
 
     Attributes
     ----------
-    nc: str
+    id: str
         The identifier number of the organism
+    version: int
+        The version of the document from which it was obtained
     sequence: str
         The genome sequence of the organism
     description: str
@@ -54,86 +56,118 @@ class Organism:
         organism
 
     """
-    def __init__(self, nc=None, sequence=None, description=None):
+    def __init__(self, id=None, version=None, sequence=None, description=None):
         """
         Parameters
         ----------
-        nc : str
+        id : str
             The identifier number
+        version : int
+            The number version of the document 
+            from which it was obtained
         sequence : str
             The complete gnome
         description : str, optional
             The name or the description of the organism
         """
-        if (nc or sequence or description):    
-            if not isinstance(nc, str):
-                raise TypeError("The NC must be a string")
+        if (id or version or sequence or description):    
+            if not isinstance(id, str):
+                raise TypeError("The identifier must be a integer")
+            elif not isinstance(version, int):
+                raise TypeError("The version must be a string")
             elif not isinstance(sequence, str):
                 raise TypeError("The sequence must be a string")
             elif not isinstance(description, str):
                 raise TypeError("The description must be a string")
-        self.nc = nc
+        self.id = id
+        self.version = version
         self.sequence = sequence
         self.description = description
     
     def __eq__(self, anorganism):
         """
         From another organism (anorganism) compares
-        the nc number, if is equal the result is true.
+        the identifier number, if is equal the result is true.
 
         Parameters
         ----------
-        anorganism : Organism object
+        anorganism : Organism
             The organism to compare
         """
         if isinstance(anorganism, Organism) == False:
             raise TypeError("Just can compare organisms with organisms")
-        if self.nc == anorganism.nc:
+        if (self.id == anorganism.id):
             return True
         else:
             return False
 
     def printOrganism(self):
+        """
+        Prints all information of the Organism on screen
+        """
         print("Description: "+self.description)
         print("ID: "+self.nc)
         print("Sequence: "+self.sequence)
     
-    def printID(self):
-        print("ID: "+self.nc)
+    def printId(self):
+        """
+        Prints just the id of the Organism on screen
+        """
+        if self.version:
+            print("ID: "+self.id+'.'+str(self.version))
+        else:
+            print("ID: "+self.id)
 
-    def createByJSON(self, filename, collection='organisms', nc='nc', 
+    def createByJSON(self, filename, collection='organisms', id='id', 
     sequence='sequence', description='description'):
         with open(filename) as handle:
             data = json.load(handle)
             for p in data[collection]:
-                self.nc = p[nc]
+                aux = p[id].split('.')
+                self.id = aux[0]
+                try:
+                    self.version = int(aux[1])
+                except IndexError:
+                    self.version = None
                 self.sequence = p[sequence]
                 self.description = p[description]
                 return
 
     def createByFASTA(self, filename):
-        nc_pattern = re.compile(r'NC_[0-9]*\.?[0-9]*')
+        id_pattern = re.compile(r'(?<=>)[A-Z_]*[0-9]*\.?[0-9]*')
         with open(filename, "rU") as handle:
             for record in SeqIO.parse(handle, "fasta"):
-                nc = re.search(nc_pattern, record.id)
-                if nc:
-                    self.nc = nc.group(0)
+                id = re.search(id_pattern, record.id)
+                if id:
+                    id = id.group(0)
+                    id = id.split('.')
+                    self.id = id[0]
+                    try:
+                        self.version = int(id[1])
+                    except IndexError:
+                        self.version = None
                 else:
-                    print("NC not found")
+                    print("Identifier not found")
                     return
                 self.description = record.description
                 self.sequence = str(record.seq)
                 return
-    
+
     def createByCLUSTAL(self, filename):
-        nc_pattern = re.compile(r'NC_[0-9]*\.?[0-9]*')
+        id_pattern = re.compile(r'(?<=>)[A-Z_]*[0-9]*\.?[0-9]*')
         with open(filename, "rU") as handle:
-            for record in SeqIO.parse(handle, 'clustal'):
-                nc = re.search(nc_pattern, record.id)
-                if nc:
-                    self.nc = nc.group(0)
+            for record in SeqIO.parse(handle, "clustal"):
+                id = re.search(id_pattern, record.id)
+                if id:
+                    id = id.group(0)
+                    id = id.split('.')
+                    self.id = id[0]
+                    try:
+                        self.version = int(id[1])
+                    except IndexError:
+                        self.version = None
                 else:
-                    print("NC not found")
+                    print("Identifier not found")
                     return
                 self.description = record.description
                 self.sequence = str(record.seq)
@@ -141,12 +175,16 @@ class Organism:
     
     def exportToCLUSTAL(self, route=None):
         if not (route):
-            route="NC_"+str(self.nc)+".aln"
+            route = self.id + ".aln"
+        if self.version:
+            version = '.'+str(self.version)
+        else:
+           version = ''
         record = SeqRecord(
             Seq(self.sequence),
-            id="NC_"+str(self.nc),
-            name=self.description,
-            description=self.description
+            id = self.id+version,
+            name = self.description,
+            description = self.description
         )
         sequences = []
         sequences.append(record)
@@ -156,12 +194,16 @@ class Organism:
 
     def exportToFASTA(self, route=None):
         if not (route):
-            route="NC_"+str(self.nc)+".fasta"
+            route = self.id + ".fasta"
+        if self.version:
+            version = '.'+str(self.version)
+        else:
+           version = ''
         record = SeqRecord(
             Seq(self.sequence),
-            id="NC_"+str(self.nc),
-            name=self.description,
-            description=self.description
+            id = self.id+version,
+            name = self.description,
+            description = self.description
         )
         sequences = []
         sequences.append(record)
@@ -170,11 +212,15 @@ class Organism:
 
     def exportToJSON(self, route=None):
         if not (route):
-            route = "NC_"+str(self.nc)+".json"
+            route = self.id + ".json"
+        if self.version:
+            version = '.'+str(self.version)
+        else:
+           version = ''
         data = {}
         data['organisms'] = []
         data['organisms'].append({
-            'nc': str(self.nc),
+            'id': self.id+version,
             'sequence': self.sequence,
             'description': self.description
         })
@@ -183,15 +229,21 @@ class Organism:
     
     @classmethod
     def createManyByFASTA(cls, filename, organisms=[]):
-        nc_pattern = re.compile(r'NC_[0-9]*\.?[0-9]*')
+        id_pattern = re.compile(r'\A[A-Z_]*[0-9]*\.?[0-9]*')
         with open(filename, "rU") as handle:
             for record in SeqIO.parse(handle, "fasta"):
                 x = Organism()
-                nc = re.search(nc_pattern, record.id)
-                if nc:
-                    x.nc = nc.group(0)
+                id = re.search(id_pattern, record.id)
+                if id:
+                    id = id.group(0)
+                    id = id.split('.')
+                    x.id = id[0]
+                    try:
+                        x.version = int(id[1])
+                    except IndexError:
+                        x.version = None
                 else:
-                    print("NC not found")
+                    print("ID not found")
                     continue
                 x.description = record.description
                 x.sequence = str(record.seq)
@@ -200,15 +252,21 @@ class Organism:
 
     @classmethod
     def createManyByCLUSTAL(cls, filename, organisms=[]):
-        nc_pattern = re.compile(r'NC_[0-9]*\.?[0-9]*')
+        id_pattern = re.compile(r'(?<=>)[A-Z_]*[0-9]*\.?[0-9]*')
         with open(filename, "rU") as handle:
             for record in SeqIO.parse(handle, "clustal"):
                 x = Organism()
-                nc = re.search(nc_pattern, record.id)
-                if nc:
-                    x.nc = nc.group(0)
+                id = re.search(id_pattern, record.id)
+                if id:
+                    id = id.group(0)
+                    id = id.split('.')
+                    x.id = id[0]
+                    try:
+                        x.version = int(id[1])
+                    except IndexError:
+                        x.version = None
                 else:
-                    print("NC not found")
+                    print("ID not found")
                     continue
                 x.description = record.description
                 x.sequence = str(record.seq)
@@ -216,13 +274,18 @@ class Organism:
         return organisms
 
     @classmethod
-    def createManyByJSON(cls, filename, organisms=[], collection='organisms', nc='nc', 
-    sequence='sequence', description='description'):
+    def createManyByJSON(cls, filename, organisms=[], collection='organisms', 
+    id='id', sequence='sequence', description='description'):
         with open(filename) as handle:
             data = json.load(handle)
             for p in data[collection]:
                 x = Organism()
-                x.nc = p[nc]
+                aux = p[id].split('.')
+                x.id = aux[0]
+                try:
+                    x.version = int(aux[1])
+                except IndexError:
+                    x.version = None
                 x.sequence = p[sequence]
                 x.description = p[description]
                 organisms.append(x)
